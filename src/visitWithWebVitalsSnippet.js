ff0e4020b131cc75d27b7156b10543f82a3cc19d
Jon Miller <jonmiller@redventures.com>
2021-11-02 16:31:28 -0400

    bug: fix script injection when head tag has whitespace or attributes

--- src/visitWithWebVitalsSnippet.js
+++ src/visitWithWebVitalsSnippet.js
@@ -3,7 +3,11 @@ const { WEB_VITALS_SNIPPET } = require("./constants");
 const visitWithWebVitalsSnippet = (url) => {
   cy.intercept(url, (req) => {
     req.continue((res) => {
-      res.send(res.body.replace("<head>", `<head>${WEB_VITALS_SNIPPET}`));
+      const body = res.body.replace(
+        /<head(>|.*?[^?]>)/g,
+        `<head$1${WEB_VITALS_SNIPPET}`
+      );
+      res.send(body);
     });
   });
 
