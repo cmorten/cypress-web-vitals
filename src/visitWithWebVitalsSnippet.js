3dc58cf69cdc0552b409e46750fa06603bd64f17
cmorten <craig.morten@hotmail.co.uk>
2022-04-02 12:22:01 +0100

    fix: only target first <head> reference

--- src/visitWithWebVitalsSnippet.js
+++ src/visitWithWebVitalsSnippet.js
@@ -4,7 +4,7 @@ const visitWithWebVitalsSnippet = (url) => {
   cy.intercept({ method: "GET", url, times: 1 }, (req) => {
     req.continue((res) => {
       const body = res.body.replace(
-        /<head(>|.*?[^?]>)/g,
+        /<head(>|.*?[^?]>)/,
         `<head$1${WEB_VITALS_SNIPPET}`
       );
       res.send(body);
