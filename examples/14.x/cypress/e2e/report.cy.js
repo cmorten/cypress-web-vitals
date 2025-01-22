c9c4f3ccf8d32003f41723ce6b273dfa4c669c48
Bill Glesias <bglesias@gmail.com>
2025-01-22 02:44:24 -0500

    feat: support Cypress 14 (#36)

---
+++ examples/14.x/cypress/e2e/report.cy.js
@@ -0,0 +1,27 @@
+const customThresholds = {
+  lcp: 2500,
+  fid: 100,
+  cls: 0.1,
+};
+
+describe("cy.vitals() command not using the defaults", () => {
+  it("should meet the custom provided Web Vitals thresholds", () => {
+    cy.vitals({
+      url: "https://www.google.com/",
+      thresholds: customThresholds,
+      onReport(report) {
+        expect(report.thresholds).to.equal(customThresholds);
+        expect(report.results).to.have.property("lcp");
+        expect(report.results).to.have.property("fid");
+        expect(report.results).to.have.property("cls");
+        expect(report.results).to.have.property("fcp");
+        expect(report.results).to.have.property("ttfb");
+        expect(report.results).to.have.property("inp");
+
+        cy.log("------ onReport values ------");
+        cy.log(JSON.stringify(report, undefined, 2));
+        cy.log("-----------------------------");
+      },
+    });
+  });
+});
