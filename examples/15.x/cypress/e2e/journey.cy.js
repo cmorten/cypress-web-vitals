c730c02207597739d475ff3d3e11d51624010e1e
cmorten <craig.morten@hotmail.co.uk>
2025-11-16 20:15:50 +0000

    feat: cypress 15 support

---
+++ examples/15.x/cypress/e2e/journey.cy.js
@@ -0,0 +1,35 @@
+describe("cy.startVitalsCapture() and cy.reportVitals() journey", () => {
+  it("should meet the custom provided Web Vitals thresholds including INP", () => {
+    cy.startVitalsCapture({
+      url: "https://www.google.com/",
+    });
+
+    // Handle CI vs local having differences in whether cookie banner is
+    // present (the problem with testing against a site you don't own!)
+    cy.get("body").then(($body) => {
+      const $button = $body.find("button:contains('Accept all')");
+
+      if ($button.length) {
+        cy.wrap($button, { log: false }).realClick();
+      }
+    });
+
+    cy.findByRole("button", { name: "Google apps" }).realClick();
+    cy.findByText("Gmail").should("be.visible");
+    cy.findByRole("button", { name: "Google apps" }).realClick();
+    cy.findByRole("combobox", { name: "Search" }).realClick().realClick();
+    cy.findByRole("listbox").should("be.visible");
+
+    cy.reportVitals({
+      thresholds: {
+        lcp: 2500,
+        fid: 100,
+        cls: 0.1,
+        fcp: 1800,
+        ttfb: 600,
+        // Google doesn't pass their own Good consistently
+        inp: 500,
+      },
+    });
+  });
+});
